bf30f3074fb8119b0fbb5e2b4951fd1c58a1d60a
sibvip <sanou020@gmail.com>
2018-02-03 03:13:47 +0000

    Update CryptoNoteConfig.h
    seed node ip

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
+  "sibtech.ddns.net:19114",
   //"your_seed_ip2.com:8080",
 };
 
